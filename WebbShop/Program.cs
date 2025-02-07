cc8c16d47429c7b81960eacbfd4636b5c9a2cb18
Marcus <marcus.johnsson@campusnykoping.se>
2025-02-07 11:53:18 +0100

    psuh

--- WebbShop/Program.cs
+++ WebbShop/Program.cs
@@ -5,20 +5,9 @@
 
         static async Task Main(string[] args)
         {
-            // Micke Frågor! 'using' statement can be simplified
+            
 
-            // visual klagar (blåt medelande) den föredrar >>>  var myDb = new MyDbContext()
-
-            // istället för using (var myDb = MyDbContext()) { bla bla bla.....}  
-
-            //------
-
-            // Kan/ska man använda var istället för typ T och andra kommandon?
-
-            //-------------
-
-            // createUserBox.Add("   Addres:                    "); eller  createUserBox.Add("   Addres: "); Spelar det någon större roll i sin hel het?
-
+            
             //AddToDataBase.Run();
             
 
